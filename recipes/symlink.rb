7b19e74354cd7f5f90aff0aa651c42b4c6d1d0c6
David Higgins <david@gryphonandrook.com>
2016-05-04 14:40:12 -0600

    Recipe to symlink assets folder

---
+++ recipes/symlink.rb
@@ -0,0 +1,18 @@
+node[:deploy].each do |application, deploy|
+  release_path  = deploy[:release_path]
+  shared_path   = deploy[:shared_path]
+
+  # create shared directory for assets, if it doesn't exist
+  directory "#{shared_path}/public/assets" do
+    mode 0770
+    action :create
+    recursive true
+  end
+
+  Chef::Log.info("Symlinking #{release_path}/public/assets to #{shared_path}/shared/assets")
+
+  # symlink current deploy's asset folder to shared assets each deploy
+  link "#{release_path}/public/assets" do
+    to "#{shared_path}/public/assets"
+  end
+end
